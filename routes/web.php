01615cc8444d40cb7f028cf05074dd6e78c945ce
sachirimo <morichi.tktktk.13@gmail.com>
2024-01-13 12:13:08 +0900

    ブログ投稿の削除

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ use App\Http\Controllers\PostController;  //外部にあるPostControllerクラ�
 Route::get('/', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::post('/posts', [PostController::class, 'store']);
+Route::delete('/posts/{post}', [PostController::class,'delete']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
 Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
 Route::put('/posts/{post}', [PostController::class, 'update']);
+
 // '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
